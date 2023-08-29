e1cd18c9b01649ad1c71d991751e4b440eb287e4
nnuauroral@163.com <herohero249>
2023-08-29 23:08:48 +0800

    格式化

--- src/pages/admin/pages/uploadExample.vue
+++ src/pages/admin/pages/uploadExample.vue
@@ -1,21 +1,25 @@
 <template>
-    <div>
-        <UploadOss v-model="uploadObj.list" :path="'/'"/>
-    </div>
+  <div>
+    <UploadOss v-model="uploadObj.list" :path="'/'" />
+  </div>
 </template>
 
 <script setup>
-import UploadOss from '../../../components/UploadOss.vue';
-import { reactive, watch } from 'vue';
+  import UploadOss from '../../../components/UploadOss.vue'
+  import { reactive, watch } from 'vue'
 
-const uploadObj = reactive({
-    list: []
-})
+  const uploadObj = reactive({
+    list: [],
+  })
 
-watch(
-    () => uploadObj.list, 
+  watch(
+    () => uploadObj.list,
     (val) => {
-        console.log('val::', val)
-    }, 
+      console.log('val::', val)
+    },
     {
-        deep:true, // 深度监听        immediate:true // 立即执行    })</script>
+      deep: true,
+      immediate: true,
+    },
+  )
+</script>
